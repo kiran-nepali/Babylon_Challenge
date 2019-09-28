92ef5d8f77c76611a13772c4f11b859830b0ba96
kiran-nepali <cuziamnepali@hotmail.com>
2019-09-28 22:58:53 +0100

    layout design with cardview feature

--- app/src/main/java/com/example/babylonchallenge/view/MainActivity.kt
+++ app/src/main/java/com/example/babylonchallenge/view/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.babylonchallenge.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.babylonchallenge.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFragment()
+    }
+
+    private fun addFragment(){
+        val fragmentManager:FragmentManager = supportFragmentManager
+        val fragmentTransaction:FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragmentContainer,PostFragment())
+            .commit()
     }
 }
